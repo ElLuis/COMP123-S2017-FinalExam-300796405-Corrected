a12576076501d5d796a9caee60e7175f24a48b0c
Luis Sanchez <lsanch14@my.centennialcollege.ca>
2017-08-20 11:59:55 -0400

    Added properties to ScoreBoard class

---
+++ COMP123-S2017-FinalExam-StudentID/ScoreBoard.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMP123_S2017_FinalExam_StudentID
+{
+    class ScoreBoard
+    {
+        private TextBox _finalScoreTextBox;
+        private int _score;
+        private TextBox _scoreTextBox;
+        private int _time;
+        private TextBox _timeTextBox;
+
+        public TextBox FinalScoreTextBox
+        {
+            get
+            {
+                return _finalScoreTextBox;
+            }
+            set
+            {
+                _finalScoreTextBox = value;
+            }
+        }
+        public int Score //set ScoreTextBox.text & FinalScoreTextBox.text --> _score value
+        {
+            get
+            {
+                return _score;
+            }
+            set
+            {
+                FinalScoreTextBox.Text = value.ToString();
+                ScoreTextBox.Text = value.ToString();
+            }
+        }
+        public TextBox ScoreTextBox
+        {
+            get
+            {
+                return _scoreTextBox;
+            }
+            set
+            {
+                _scoreTextBox = value;
+            }
+        }
+    }
+}
